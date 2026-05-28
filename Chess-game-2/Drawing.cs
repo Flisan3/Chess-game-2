0b85328a9847c92ac1c82070e3ef9971feb2f8b8
Flisan <alisagers@gmail.com>
2026-05-28 22:41:03 +0200

    (Add) Fixes

--- Chess-game-2/Drawing.cs
+++ Chess-game-2/Drawing.cs
@@ -9,8 +9,7 @@ namespace Chess_game_2
 {
     internal class Drawing
     {
-        public static GameState CurrentGameState { get; set; }
-
+ 
         // Variables
         public static int SquareSize = 120;
         public static int BoardSize = 8;
